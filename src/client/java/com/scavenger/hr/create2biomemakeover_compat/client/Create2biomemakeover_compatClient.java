895b09cea392b60ad8a5e8a454105ad2389930e8
Dubravko Cvikl <dcvikl@hotmail.com>
2024-11-02 23:12:26 +0100

    Add files via upload

---
+++ src/client/java/com/scavenger/hr/create2biomemakeover_compat/client/Create2biomemakeover_compatClient.java
@@ -0,0 +1,10 @@
+package com.scavenger.hr.create2biomemakeover_compat.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Create2biomemakeover_compatClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/com/scavenger/hr/create2biomemakeover_compat/Create2biomemakeover_compat.java
@@ -0,0 +1,10 @@
+package com.scavenger.hr.create2biomemakeover_compat;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Create2biomemakeover_compat implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
